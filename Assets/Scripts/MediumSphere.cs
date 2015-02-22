45f871c5a619018c2e4e8f32e734676a8205a516
Chris James <tobaljackson@gmail.com>
2015-02-22 18:35:54 -0500

    initial commit

---
+++ Assets/Scripts/MediumSphere.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MediumSphere : MonoBehaviour {
+	public int speed;
+	public int minDistanceToOtherMedSpheres;
+
+	GameObject[] otherMedSpheres;
+	GameObject player;
+	Vector3 distanceToPlayer;
+	Vector3 distanceToMedSphere;
+	int maxDistance;
+	int minDistance;
+	Rigidbody MSRigidBody;
+
+	void Start () {
+		otherMedSpheres = GameObject.FindGameObjectsWithTag ("MediumSphere");
+		MSRigidBody = GetComponent<Rigidbody> ();
+		player = GameObject.FindGameObjectWithTag("Player");
+		distanceToPlayer = player.transform.position - this.transform.position;
+		maxDistance = player.GetComponent<SphereController>().followerMaxDistance;
+		minDistance = player.GetComponent<SphereController> ().followerMinDistance;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		foreach(GameObject sphere in otherMedSpheres){
+			distanceToMedSphere = sphere.transform.position - this.transform.position;
+			if (distanceToMedSphere.magnitude < this.minDistanceToOtherMedSpheres){
+				MSRigidBody.AddForce(-1 * distanceToPlayer * speed * Time.fixedDeltaTime);
+			}
+		}
+
+		distanceToPlayer = player.transform.position - this.transform.position;
+		if (distanceToPlayer.magnitude < minDistance) {
+			MSRigidBody.AddForce(-1 * distanceToPlayer * speed * Time.fixedDeltaTime);
+		}
+		if (distanceToPlayer.magnitude > maxDistance) {
+			MSRigidBody.AddForce(distanceToPlayer * speed * Time.fixedDeltaTime);
+		}
+	}
+}
