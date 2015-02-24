b1a58febc0d1e7cd93dee494e16e35090904bd89
TobalJackson <Tobaljackson@gmail.com>
2015-02-23 19:33:00 -0500

    herp

--- Assets/Scripts/MediumSphereController.cs
+++ Assets/Scripts/MediumSphereController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MediumSphereController : MonoBehaviour {
 	public int speed;
 	public int minDistanceToOtherMedSpheres;
+	public int visionRadius;
 
 	GameObject[] otherMedSpheres;
 	GameObject player;
@@ -15,16 +16,40 @@ public class MediumSphereController : MonoBehaviour {
 	Vector3 targetLocation;
 	Vector3 distanceToTarget;
 
+	GameObject[] targets;
+	ArrayList visibleTargets;
+
+	//ArrayList mySmallSpheres;
+	//public GameObject smallSphere1;
+	//GameObject[] smallSpheres;
+	//int maxFollowers;
+
 	bool isEngaged;
 
 	void Start () {
+		//mySmallSpheres = new ArrayList ();
 		otherMedSpheres = GameObject.FindGameObjectsWithTag ("MediumSphere");
 		MSRigidBody = GetComponent<Rigidbody> ();
 		player = GameObject.FindGameObjectWithTag("Player");
 		distanceToPlayer = player.transform.position - this.transform.position;
 		maxDistance = player.GetComponent<BigSphereController>().followerMaxDistance;
 		minDistance = player.GetComponent<BigSphereController> ().followerMinDistance;
+		targets = GameObject.FindGameObjectsWithTag ("Target");
+		visibleTargets = new ArrayList ();
+		//maxFollowers = 1;
+		//smallSpheres = GameObject.FindGameObjectsWithTag ("SmallSphere");
+		//mySmallSpheres.Add (smallSphere1.gameObject);
 
+		//if (mySmallSpheres.Count < maxFollowers) {
+		//	foreach (GameObject sphere in smallSpheres){
+		//		if (!sphere.GetComponent<smallSphereController>().getHasParent()){
+		//			sphere.GetComponent<smallSphereController>().setParent(this.gameObject);
+		//			this.mySmallSpheres.Add (sphere);
+		//		}
+		//	}
+		//}
+	}
+	void Awake(){
 
 	}
 	
@@ -42,7 +67,22 @@ public class MediumSphereController : MonoBehaviour {
 		//}
 
 		pursueTarget();
-	
+		updateTargetVisibility ();
+		if (targetInRange()) {
+			foreach (GameObject target in visibleTargets){
+				target.GetComponent<TargetController>().takeDamage(10);
+			}
+			//if (mySmallSpheres.Count != 0){
+			//	foreach (GameObject sphere in mySmallSpheres){
+			//		this.dispatchTargetLocation(sphere, targetLocation);
+			//	}
+			//}
+		}
+		//else if(mySmallSpheres.Count != 0){
+		//	foreach (GameObject sphere in mySmallSpheres){
+		//		this.dispatchTargetLocation(sphere, this.transform.position);
+		//	}
+		//}
 	}
 	public bool getIsEngaged(){
 		return this.isEngaged;
@@ -62,6 +102,28 @@ public class MediumSphereController : MonoBehaviour {
 			}
 			if (distanceToTarget.magnitude > maxDistance) {
 				MSRigidBody.AddForce(distanceToTarget * speed * Time.fixedDeltaTime);
+			}
+		}
+	}
+	//public void addSmallSphere(GameObject sphere){
+	//	this.mySmallSpheres.Add (sphere);
+	//}
+	void dispatchTargetLocation(GameObject smallSphere, Vector3 location){
+		smallSphere.GetComponent<smallSphereController> ().setTargetLocation (location);
+	}
+	public bool targetInRange(){
+		if (this.visibleTargets.Count == 0)	return false; 
+		else return true;
+	}
+	void updateTargetVisibility(){
+		foreach (GameObject uTarget in targets) {
+			if (Mathf.Abs((uTarget.transform.position - this.transform.position).magnitude) < visionRadius){
+				if (!this.visibleTargets.Contains(uTarget)){
+					visibleTargets.Add(uTarget);
+				}
+			}
+			else if (visibleTargets.Contains (uTarget)){
+				visibleTargets.Remove (uTarget);
 			}
 		}
 	}
--- Assets/Scripts/smallSphereController.cs
+++ Assets/Scripts/smallSphereController.cs
@@ -3,13 +3,66 @@ using System.Collections;
 
 public class smallSphereController : MonoBehaviour {
 
+	public int minFollowDistance;
+	public int maxFollowDistance;
+	public int speed;
+
+	GameObject medSphereParent;
+	Rigidbody smallSphereRigid;
+	bool isEngaged;
+	Vector3 targetLocation;
+	GameObject[] medSpheres;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		medSpheres = GameObject.FindGameObjectsWithTag ("MediumSphere");
+		//attachToNearest (medSpheres);
+
+		isEngaged = false;
+		targetLocation = medSphereParent.transform.position;
+		medSphereParent = this.transform.parent.gameObject;
+		smallSphereRigid = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		pursueTarget ();
 	}
+
+	void pursueTarget(){
+		Vector3 distanceToTarget = this.targetLocation - this.transform.position;
+		if (distanceToTarget.magnitude < this.minFollowDistance) {
+			smallSphereRigid.AddForce (distanceToTarget * speed * Time.fixedDeltaTime);
+		}
+		if (distanceToTarget.magnitude < this.maxFollowDistance) {
+			smallSphereRigid.AddForce (-1 * distanceToTarget * speed * Time.fixedDeltaTime);
+		}
+	}
+	public void setTargetLocation(Vector3 location){
+		this.targetLocation = location;
+	}
+	public bool getHasParent(){
+		if (this.medSphereParent == null)
+			return false;
+		else
+			return true;
+	}
+	public void setParent(GameObject medSphere){
+		medSphereParent = medSphere;
+	}
+
+	//void attachToNearest(GameObject[] spheres){
+	//	float minDistance = 9999;
+	//	foreach (GameObject sphere in spheres){
+	//		float distance = Mathf.Abs ((sphere.transform.position - this.transform.position).magnitude);
+	//		Debug.Log (distance);
+	//		if (distance < minDistance){
+	//			minDistance = distance;
+	//			this.medSphereParent = sphere;
+	//		}
+	//	}
+	//	Debug.Log (medSphereParent == null);
+	//	this.medSphereParent.GetComponent<MediumSphereController> ().addSmallSphere (this.gameObject);
+	//}
 }
